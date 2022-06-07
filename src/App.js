846828727b9974ee05861fa816036cbf86aabdff
Lucas Ribeiro <martins7ribeiro@gmail.com>
2022-06-06 22:25:13 -0300

    aviso de manutenção

--- src/App.js
+++ src/App.js
@@ -1,46 +1,48 @@
 import React from 'react';
-import {
-  BrowserRouter,
-  Switch,
-  Route,
-} from 'react-router-dom';
-
-import {
-  About,
-  Navbar,
-  NotFound,
-  Footer,
-  Projects,
-  Stack,
-  Welcome,
-} from './components';
-
-// CSS Imports
-import './css/styles.css';
-import './css/navbar.css';
-import './css/stack.css';
-import './css/footer.css';
-import './css/welcome.css';
-import './css/about.css';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <BrowserRouter>
-          <Navbar />
-          <div className="content-container">
-            <Switch>
-              <Route exact path="/" component={Welcome} />
-              <Route path="/about" component={About} />
-              <Route path="/projects" component={Projects} />
-              <Route component={NotFound} />
-            </Switch>
-          </div>
-          <Stack />
-          <Footer />
-        </BrowserRouter>
-      </div>
+      <>
+        <style>
+          {`
+            main {
+              display: flex;
+              flex-flow: column nowrap;
+              justify-content: center;
+              align-items: center;
+              font-family: monospace;
+              width: 80vw;
+              margin: 0 auto;
+            }
+
+            p {
+              width: 80%;
+              font-size: 18px;
+            }
+
+            strong {
+              font-size: 22px;
+            }
+          `}
+        </style>
+        <main>
+          <h1> Em manutenção | Under maintenance</h1>
+
+          <p>
+            <strong>PT-BR</strong> <br />
+            Com o objetivo de expor meu trabalho da melhor maneira possível,
+            o site está em um profundo processo de atualização. Em breve,
+            tudo isso aqui vai estar com uma cara completamente nova.
+          </p>
+
+          <p>
+            <strong>EN</strong> <br />
+            Aiming at the best way to show my work here, the site's under a deep
+            update process. Soon, this portfolio is going to be brand new.
+          </p>
+        </main>
+      </>
     )
   }
 }
